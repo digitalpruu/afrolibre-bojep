8c638ccaff5797d0579a7c31dfaf8d3ee2988ef7
kevinpicon01 <digitalpruu@gmail.com>
2024-10-11 17:32:23 -0500

    fix title

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -1,4 +1,4 @@
-const title = "AFROLIBRE (BOROJEP)";
+const title = "Fundación afrocolombiana liberal desarrollo social afrolibre.";
 const email = "wilsonpalaciosprofe1976@gmail.com";
 const mensajeWhatsApp = encodeURIComponent(
     "¡Hola! Estoy interesado en conocer más sobre los productos de " + title + " y me gustaría obtener más información. ¡Gracias!"
